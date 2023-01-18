48c031108e933afcde7346af385470c84a30643d
Edward Gandar <65567827+EdwardG13@users.noreply.github.com>
2023-01-18 20:44:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-```javascript
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles-js config loaded');
 });
-```
